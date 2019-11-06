4663e905e1736a7782d85aaa0f2ec7c24680462c
Mirbek <mirbek@MacBook-Pro-Mirbek.local>
2019-11-06 10:32:55 +0100

    Updated function

--- Fibonacci_function.R
+++ Fibonacci_function.R
@@ -3,17 +3,18 @@ n2 <- 1
 count <- 2
 
 fibonacci <- function(n) {
-  print(n1)
-  print(n2)
-    while(count < n)
+  if (n <= 1) 
+      return(n1)
+  else
+    print(n1)
+    print(n2)
+      while(count < n)
       {
       nsum <- n1 + n2
       print(nsum)
       n1 <- n2
       n2 <- nsum
       count <- count + 1
-    
-    }
+      }
 }
-
-fibonacci(100)
+fibonacci(-6)
